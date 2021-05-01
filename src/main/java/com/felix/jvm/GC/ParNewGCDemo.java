a19e1a912db7fc5bda855c860a408520f65f7baf
FelixCliton <liujie3@yidian-inc.com>
2021-05-01 17:19:14 +0800

    update gc

---
+++ src/main/java/com/felix/jvm/GC/ParNewGCDemo.java
@@ -0,0 +1,26 @@
+package com.felix.jvm.GC;
+
+import java.util.Random;
+
+/**
+ * @author liujie3@yidian-inc.com
+ * @version 1.0
+ * @date 2021/5/1 9:17
+ * @desc:
+ *
+ */
+public class ParNewGCDemo {
+
+    /**
+     * -XX:+PrintCommandLineFlags -XX:+UseParNewGC -XX:+PrintGCDetails -Xms10m -Xmx10m
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str = "hahhah";
+        while (true) {
+            str += str + new Random().nextInt(7777777) + new Random().nextInt(999999999);
+            str.intern();
+        }
+    }
+
+}
